a8d5bda0c7ad4265676a41616506fc775adc10a4
cinda87 <j.c.bietz@gmail.com>
2025-04-13 18:08:13 -0600

    feat: add footer with credits and external links

---
+++ src/Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="custom-footer">
+      <p>
+        This project was{" "}
+        <a
+          href="https://www.linkedin.com/in/jacinda-bietz-3158a0338/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          coded by Jacinda Bietz
+        </a>
+        , is{" "}
+        <a
+          href="https://github.com/CindaCodes/dictionary-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://wordly-wise.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+        .
+      </p>
+    </footer>
+  );
+}
