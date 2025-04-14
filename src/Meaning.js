10804336bb841734dc7c5a9fa83547e8d32762d0
cinda87 <j.c.bietz@gmail.com>
2025-04-14 13:41:23 -0600

    feat: structured definitions with synonyms and antonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,9 +9,35 @@ export default function Meaning(props) {
 
   return (
     <div className="Meaning">
-      <h3>{meaning.partOfSpeech}</h3>
-      <p>{meaning.definition}</p>
-      {meaning.example && <em>{meaning.example}</em>}
+      <div className="definition-box">
+        <h4>{meaning.partOfSpeech}</h4>
+        <strong>Definition:</strong> {meaning.definition}
+        {meaning.example && (
+          <p>
+            <strong>Example:</strong> <em>{meaning.example}</em>
+          </p>
+        )}
+        {meaning.synonyms && meaning.synonyms.length > 0 && (
+          <div className="synonym-list">
+            <strong>Synonyms:</strong>
+            <ul>
+              {meaning.synonyms.map((synonym, index) => (
+                <li key={index}>{synonym}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {meaning.antonyms && meaning.antonyms.length > 0 && (
+          <div className="antonym-list">
+            <strong>Antonyms:</strong>
+            <ul>
+              {meaning.antonyms.map((antonym, index) => (
+                <li key={index}>{antonym}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
