3ff89200b1f1a6474f674d740cccca48b7e142b7
cinda87 <j.c.bietz@gmail.com>
2025-04-14 13:38:53 -0600

    feat: replace logo with custom app title + subtitle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo-shecodes.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 import Footer from "./Footer";
@@ -7,8 +6,10 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header text-center">
+          <h1 className="App-title">Wordly Wise</h1>
+          <h3>Your go-to dictionary for all things word-related!</h3>
+          <hr />
         </header>
         <main>
           <Dictionary />
