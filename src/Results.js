7cc999b47dc04e879fc9b5b44fe372c158ba2847
cinda87 <j.c.bietz@gmail.com>
2025-04-15 17:47:05 -0600

    🎧 feat: integrated Phonetics component in Results with improved layout and filtering

--- src/Results.js
+++ src/Results.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
-  console.log(props.results);
-
-  if (props.results && props.results.meanings) {
+  if (props.results) {
     return (
-      <div className="Results mt-4">
-        <h2 className="text-center">
-          <strong>{props.results.word}</strong> /  
-          {" "}{props.results.phonetic}
-        </h2>
+      <div className="Results">
+        <h2 className="highlight display-6 text-center mt-3">{props.results.word}</h2>
+        <div className="hightlight">
+          {" "}
+          {props.results.phonetics
+            .filter((p) => p.text || p.audio)
+            .slice(0, 1)
+            .map((phonetic, index) => (
+              <Phonetics phonetic={phonetic} key={index} />
+            ))}
+        </div>
         {props.results.meanings.map((meaning, index) => (
           <Meaning meaning={meaning} key={index} />
         ))}
