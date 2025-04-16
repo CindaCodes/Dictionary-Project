c3703cd9b47cfb8d80a987fea28b2cfefd4cb90a
cinda87 <j.c.bietz@gmail.com>
2025-04-16 16:22:43 -0600

    cleanup: simplify phonetics by limiting to one audio

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,24 +3,19 @@ import React from "react";
 export default function Phonetics(props) {
   if (!props.phonetics || props.phonetics.length === 0) return null;
 
+  const firstAudio = props.phonetics.find((phonetic) => phonetic.audio)?.audio;
+
+  if (!firstAudio) return null;
+
   return (
     <div className="Phonetics">
-      {props.phonetics.map((phonetic, index) => {
-        const { text, audio } = phonetic;
-        if (!text && !audio) return null;
-
-        return (
-          <div className="Phonetic text-center my-3" key={index}>
-            {audio && (
-              <div className="phonetic-audio mt-2">
-                <audio controls src={audio}>
-                  Your browser does not support the audio element.
-                </audio>
-              </div>
-            )}
-          </div>
-        );
-      })}
+      <div className="Phonetic text-center my-3">
+        <div className="phonetic-audio mt-2">
+          <audio controls src={firstAudio}>
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+      </div>
     </div>
   );
 }
